012d99fa4322121eb0f54a3307b5bee114ece789
Angelo Lustosa de Figueiredo Filho <01337098337@DIGITALCOLLEGE.LOCAL>
2024-08-22 18:58:34 -0300

    Async/await

---
+++ async-await.js
@@ -0,0 +1,19 @@
+/* console.log('1-inicio');
+
+const getUsers = () => {
+    console.log('inicio-getUsers');
+    fetch('https://jsonplaceholder.typicode.com/users')
+     .then((response) => response.json())
+     .then(data => {
+        console.log('meio-data');
+        console.log(data)
+    });
+    console.log('fim-getUsers');
+}
+
+getUsers();
+
+console.log('4-fim'); */
+
+
+//// com async / await
